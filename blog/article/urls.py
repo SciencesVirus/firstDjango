37b2bbbed1b221cb25807b8e5622480a92bccf19
virus <virus@virus>
2022-07-10 16:46:46 +0800

    7/10 chep10

--- blog/article/urls.py
+++ blog/article/urls.py
@@ -5,4 +5,10 @@ from article import views
 app_name = 'article'
 urlpatterns = [
     path('', views.article, name='article'),
+    path('articleCreate/', views.articleCreate, name='articleCreate'),
+    path('articleUpdate/<int:articleId>/', views.articleUpdate, name='articleUpdate'),
+    path('articleDelete/<int:articleId>/', views.articleDelete, name='articleDelete'),
+    path('articleRead/<int:articleId>/', views.articleRead, name='articleRead'),
+    path('articleSearch/', views.articleSearch, name='articleSearch'),
+    
 ]
